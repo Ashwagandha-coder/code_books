2256c3bd11ec65141af3b8017502102aec1ab4cf
Ashwagandha-coder <max.cacz@yandex.ru>
2023-02-05 22:45:18 +0500

    [add] add binary tree

---
+++ DoSome/ComputerScienceForProgrammer/BinaryTree.kt
@@ -0,0 +1,89 @@
+class BinaryTree {
+
+    private var root: Node? = null
+
+
+    fun add(value: Int) {
+
+        if (root == null) {
+            root = Node(value)
+        } else {
+            var currentNode = root
+            var parentNode: Node
+
+            while (true) {
+                parentNode = currentNode!!
+
+                if (value == root?.getValue()) {
+                    return
+                } else if (value < root?.getValue()!!) {
+
+                    currentNode = currentNode.isLeft()
+                    if (currentNode == null) {
+
+                        val node = Node(value)
+                        parentNode.left(node)
+                        return
+                    }
+
+                } else {
+
+                    currentNode = currentNode.isRight()
+
+                    if (currentNode == null) {
+
+                        val node = Node(value)
+                        parentNode.right(node)
+                        return
+                    }
+
+                }
+
+
+            }
+
+        }
+
+    }
+
+    override fun toString(): String {
+        return "BinaryTree(root=$root)"
+    }
+
+
+    private class Node(
+        private var value: Int = 0,
+        private var left: Node? = null,
+        private var right: Node? = null
+    ) {
+
+        fun left(node: Node) {
+            this.left = node
+        }
+
+        fun right(node: Node) {
+            this.right = node
+        }
+
+        fun value(value: Int) {
+            this.value = value
+        }
+
+        fun getValue() = value
+
+
+        fun isLeft() = left
+
+        fun isRight() = right
+
+
+
+        override fun toString(): String {
+            return "Node(value=$value, left=$left, right=$right)"
+        }
+
+
+    }
+
+
+}
